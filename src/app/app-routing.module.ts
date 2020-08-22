15b9d3eb388eadc4922c24eb2681fedde6637aa3
Abir EL HALIMI <45515682+abirelhalimi@users.noreply.github.com>
2020-08-22 15:54:14 +0200

    full migration

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {ProjectComponent} from './project/project.component';
+import {ResumeComponent} from './resume/resume.component';
+import {ContactComponent} from './contact/contact.component';
+import {PreviewComponent} from './preview/preview.component';
+
+const routes: Routes = [
+  {path: 'aboutme', component: AboutComponent},
+  {path: '', redirectTo: 'aboutme', pathMatch: 'full'},
+  {path: 'projects', component: ProjectComponent},
+  {path: 'resume', component: ResumeComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'preview', component: PreviewComponent},
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
+import {RouterModule} from '@angular/router';
+import {AppRoutingModule} from './app-routing.module';
+import { PreviewComponent } from './preview/preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    ProjectComponent,
+    ResumeComponent,
+    ContactComponent,
+    PreviewComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
