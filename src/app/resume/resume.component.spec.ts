0e062ed9fb0ce34b61380475f68a1f847c6b415f
Abir EL HALIMI <45515682+abirelhalimi@users.noreply.github.com>
2024-10-01 15:20:31 +0200

    update - code cleanup

--- src/app/resume/resume.component.spec.ts
+++ src/app/resume/resume.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResumeComponent } from './resume.component';
 
@@ -6,7 +6,7 @@ describe('ResumeComponent', () => {
   let component: ResumeComponent;
   let fixture: ComponentFixture<ResumeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ResumeComponent ]
     })
